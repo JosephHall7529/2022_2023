3a3576785eabdcd0a82bd5d1bbffbdecf3e1d61b
Joe <43113966+JosephHall7529@users.noreply.github.com>
2023-05-31 13:47:06 +0200

    deviation Ids

--- confinement_time/Karina.jl
+++ confinement_time/Karina.jl
@@ -9,8 +9,8 @@ include("/Users/joe/Project/Coding/J_Major_Radius_22_06_08/src/Compare.jl")
 
 ## DB2P8, DB5
 
-DB2P8_karina = H_mode_data("/Users/joe/.data/Multi_Machine_Fusion_Data_ITER_21_03_01/DB52P3_ed_!S.csv", :DB2P8)
-SELDB5_karina =  H_mode_data("/Users/joe/.data/Multi_Machine_Fusion_Data_ITER_21_03_01/DB52P3_ed_!S.csv", :SELDB5) 
+DB2P8_karina = H_mode_data("/Users/joe/.data/Multi_Machine_Fusion_Data_ITER_21_03_01/DB52P3_ed.csv", :DB2P8)
+SELDB5_karina =  H_mode_data("/Users/joe/.data/Multi_Machine_Fusion_Data_ITER_21_03_01/DB52P3_ed.csv", :SELDB5) 
 
 
 for (field, field_name) in Iterators.zip([:DB2P8, :SELDB5], [:DB2P8, :DB5])
@@ -23,3 +23,5 @@ end
 DB2P8_SELDB5_comp_karina = Compare(DB2P8, SELDB5)
 ids = DataFrame(:id => DB2P8_SELDB5_comp_karina.unique_df2.id)
 CSV.write("/Users/joe/Project/PhD/Intern/EP_Masters_Students_2022/2022_2023/confinement_time/data/new_point_ids.csv", ids)
+
+SELDB5.original_space.ELMy.ENBI |> histogram
